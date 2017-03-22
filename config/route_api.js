a85a0d91f8e7c4a84affa78e0c60675f11957b0d
fabien thetis <fabien2.thetis@gmail.com>
2017-03-22 07:45:07 +0100

    [payment]: add route for paypal payment flow

--- config/route_api.js
+++ config/route_api.js
@@ -1,6 +1,13 @@
 module.exports.routes = {
 
-    'POST    /product/save_image/:idProduct': 'api/UploadController.saveImageProduct'
+    'POST    /product/save_image/:idProduct': 'api/UploadController.saveImageProduct',
+
+    '/payment/paypal/pay': 'api/paymentController.paypalPay',
+
+    '/payment/paypal/execute': 'api/paymentController.paypalExecute'
+
+
+
 };
 
 
