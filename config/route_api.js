942b26a835915e470218557f6c2563af19de316b
fabien thetis <fabien2.thetis@gmail.com>
2017-03-25 19:13:04 +0100

    [route]: add the route to manage the double check of paypal

--- config/route_api.js
+++ config/route_api.js
@@ -6,7 +6,11 @@ module.exports.routes = {
 
     '/payment/paypal/execute/success/:idPayment': 'api/paymentController.paypalExecuteSuccess',
 
-    '/payment/paypal/execute/cancel/:idPayment': 'api/paymentController.paypalExecuteCancel'
+    '/payment/paypal/execute/cancel/:idPayment': 'api/paymentController.paypalExecuteCancel',
+
+    '/payment/paypal/execute/confirmation/success/:idPayment': 'api/paymentController.paypalExecuteConfirmationSuccess', // double confirmation paypal step
+
+    '/payment/paypal/execute/confirmation/error/:idPayment': 'api/paymentController.paypalExecuteConfirmationError' // double confirmation paypal step
 
 
 };
