27a11e9f75b588d4427913c6825ae3e8481d46dd
fabien thetis <fabien2.thetis@gmail.com>
2017-05-20 14:38:28 +0200

    implement the delete method for product

--- api/services/CoreDeleteDbService.js
+++ api/services/CoreDeleteDbService.js
@@ -6,6 +6,8 @@ var port = sails.config.connections.mongodbServer.port;
 var database = sails.config.connections.mongodbServer.database;
 var urlConnection = "mongodb://" + host + ":" + port + '/' + database;
 
+var ObjectId = require('mongodb').ObjectID;
+
 getValueFromArray = function (data, element, type) {
 
     //return 10;
@@ -32,13 +34,19 @@ getValueFromArray = function (data, element, type) {
 
             var MongoClient = require('mongodb').MongoClient;
             console.log('CoreDeleteDbService - url connexion ', urlConnection);
+            console.log('coredeleteservice - idProduct', productId);
 
             MongoClient.connect(urlConnection).then(function (db) {
 
-                if ( sails.config.demoMode != 1 ) {
+                if (sails.config.demoMode != 1) {
 
+                    var collection = db.collection('product');
+                    collection.deleteOne({_id: ObjectId(productId)}, function (err, result) {
+
+                        console.log(err);
+                        console.log('result', result);
+                    });
                 }
-
             });
         },
 
