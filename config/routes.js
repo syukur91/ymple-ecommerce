95f612143ac3af4d8bca049c955476d5ae84d2b6
fabien thetis <fabien2.thetis@gmail.com>
2017-06-22 19:53:47 +0200

    configuration of route with core functionality

--- config/routes.js
+++ config/routes.js
@@ -22,6 +22,7 @@
 
 
 
-var route_front_end = require('./route/front/route_front');
-var route_api = require('./route/api/route_api');
-var route_back_end = require('./route/back/route_back');
+var route_front_end = require('./route/core/front/route_front');
+var route_api = require('./route/core/api/route_api');
+var route_back_end = require('./route/core/back/route_back');
+var route_back_module = require('./route/core/back/route_back_module');
