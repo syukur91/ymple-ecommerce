a5bc41c245634fb1b7dfc2b838bd4ef8f94a8ce8
fabien thetis <fabien2.thetis@gmail.com>
2017-06-22 19:50:23 +0200

    define a route dedicated to core

---
+++ config/route/core/back/route_back_module.js
@@ -0,0 +1,17 @@
+module.exports.routes = {
+
+// page to manage the modules
+    'GET    /admin/module': {view: 'core/back/module/index'},
+    'GET    /admin/module/create': 'core/back/moduleController.create',
+    'GET    /admin/module/list/': 'core/back/moduleController.list',
+    'GET    /admin/module/search': 'core/back/moduleController.search', // return the list of module to be added
+    '/admin/module/install/': 'core/back/moduleController.install', // edit a module
+    '/admin/module/edit/:nameModule': 'core/back/moduleController.edit', // edit a module
+    '/admin/module/:nameModule/edit/validation': 'core/back/moduleController.editValidation', // validate the edit of one module, update the configuration
+
+    '/admin/module/inactivate/:nameModule': 'core/back/moduleController.inactivate', // inactivate a module
+
+    '/admin/module/paypal/': 'core/back/moduleController.paypal',
+    '/admin/module/manager/': 'core/back/moduleController.manager',
+
+}
