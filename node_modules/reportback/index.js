969a8fb8094e1d00b3330e7ac4fccce53dd74ed4
fabien thetis <fabien2.thetis@gmail.com>
2017-06-22 19:48:09 +0200

    update node module to migration to sails 1.0

--- node_modules/reportback/index.js
+++ node_modules/reportback/index.js
@@ -2,10 +2,8 @@
  * Module dependencies
  */
 
-var _ = require('lodash')
-	, switchback = require('switchback')
-	, mergeDefaults = require('merge-defaults')
-	, captains = require('captains-log');
+var switchback = require('switchback');
+var captains = require('captains-log');
 
 
 
@@ -26,41 +24,49 @@ module.exports = Reporter;
 
 /**
  * Factory
- * 
+ *
  * @param  {Object|Function} patch
  * @param  {Object} defaultHandlers
  * @return {Reporter} a new reporter
  */
 function Reporter (patch, defaultHandlers) {
-	patch = patch || {};
-	defaultHandlers = defaultHandlers || {};
-	if ( ! (_.isFunction(patch) || _.isObject(patch)) ) {
-		throw new Error('Invalid usage: must provide a function or object.');
-	}
+  patch = patch || {};
+  defaultHandlers = defaultHandlers || {};
+  if ( typeof patch !== 'function' && (typeof patch !== 'object' || patch === null) ) {
+    throw new Error('Invalid usage: must provide a function or object.');
+  }
 
-	// Construct logger
-	var logger = captains();
-	
+  // Construct logger
+  var logger = captains();
 
-	// Construct a switchback
-	var reporter = switchback(patch, _.defaults(defaultHandlers, {
-		success: (function(){}),
-		error  : (logger.error),
-		end    : (function(){})
-	}));
 
-	// Mixin streaming / logging functionality
-	reporter.write = logger.info;
-	reporter.log   = logger;
+  // Set up implicit defaults for `defaultHandlers`
+  if (defaultHandlers.success === undefined) {
+    defaultHandlers.success = function(){};
+  }
+  if (defaultHandlers.error === undefined) {
+    defaultHandlers.error = logger.error;
+  }
+  if (defaultHandlers.end === undefined) {
+    defaultHandlers.end = function(){};
+  }
 
-	/**
-	 * Mixin `extend()` method
-	 * @param  {Object|Function} patch
-	 * @return {Reporter} a new reporter
-	 */
-	reporter.extend = Reporter;
 
-	return reporter;
+  // Construct a switchback
+  var reporter = switchback(patch, defaultHandlers);
+
+  // Mixin streaming / logging functionality
+  reporter.write = logger.info;
+  reporter.log   = logger;
+
+  /**
+   * Mixin `extend()` method
+   * @param  {Object|Function} patch
+   * @return {Reporter} a new reporter
+   */
+  reporter.extend = Reporter;
+
+  return reporter;
 }
 
 
