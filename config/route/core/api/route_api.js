305e9c73254d01e4f7ffbbadf002fcc4a11d0ef1
fabien thetis <fabien2.thetis@gmail.com>
2017-10-10 10:35:02 +0200

    update menu style

--- config/route/core/api/route_api.js
+++ config/route/core/api/route_api.js
@@ -1,23 +1,28 @@
 module.exports.routes = {
 
-    'POST    /product/save_image/:idProduct': 'core/api/UploadController.saveImageProduct',
+
+    'POST /product/save_image/:idProduct': 'core/api/UploadController.saveImageProduct',
 
 
     '/api/save_image/module/:moduleName': 'core/api/UploadController.saveImageModule',
 
+
     '/payment/paypal/pay/:idPayment': 'core/api/paymentController.paypalPay',
+
 
     '/payment/paypal/execute/success/:idPayment': 'core/api/paymentController.paypalExecuteSuccess',
 
+
     '/payment/paypal/execute/cancel/:idPayment': 'core/api/paymentController.paypalExecuteCancel',
 
+
     '/payment/paypal/execute/confirmation/success/:idPayment': 'core/api/paymentController.paypalExecuteConfirmationSuccess', // double confirmation paypal step
+
 
     '/payment/paypal/execute/confirmation/error/:idPayment': 'core/api/paymentController.paypalExecuteConfirmationError' // double confirmation paypal step
 
 
+
+
+
 };
-
-
-
-
